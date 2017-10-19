57afd81db022a78a203a432fedb127b8d6def89d
c1042421@hik.be <c1042421@hik.be>
2017-10-19 17:55:53 +0200

    added Auto Class + timer functionality

---
+++ oefAuto/Auto.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oefAuto
+{
+    class Auto
+    {
+        private double _litersInTank;
+        private int _snelheid;
+
+        public delegate void AutoHandler(Auto sender);
+
+        public event AutoHandler AutoGestartEvent;
+        public event AutoHandler AutoGestoptEvent;
+        public event AutoHandler TankBijnaOpEvent;
+        public event AutoHandler TankChangeEvent;
+        public event AutoHandler TankOpEvent;
+
+        public Auto(double litersInTank)
+        {
+            LitersInTank = litersInTank;
+            Snelheid = 0;
+        }
+
+        public double LitersInTank {
+            get { return _litersInTank; }
+            set
+            {
+                _litersInTank = value;
+
+                TankChangeEvent?.Invoke(this);
+
+                if (value < 10 && value != 0)
+                {
+                    TankBijnaOpEvent?.Invoke(this);
+                }
+                else if (value == 0)
+                {
+                    TankOpEvent?.Invoke(this);
+                    Snelheid = 0;
+                }
+            }
+        }
+
+        public int Snelheid
+        {
+            get { return _snelheid; }
+            set
+            {
+                _snelheid = value;
+
+                (value > 0 ? AutoGestartEvent : AutoGestoptEvent)?.Invoke(sender: this);
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Snelheid: {0}\nLitersInTank: {1}", Snelheid, LitersInTank);
+        }
+    }
+}
--- oefAuto/MainWindow.xaml.cs
+++ oefAuto/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace oefAuto
 {
@@ -23,6 +24,53 @@ namespace oefAuto
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private Auto a;
+        private DispatcherTimer timer;
+
+        private void a_AutoGestartEvent(Auto sender)
+        {
+            timer.Start();
+        }
+
+        private void a_AutoGestopEvent(Auto sender)
+        {
+            timer.Stop();
+        }
+
+        private void a_TankBijnaOpEvent(Auto sender)
+        {
+            wdwAuto.Background = new SolidColorBrush(Color.FromRgb(255, 165, 0));
+        }
+
+        private void a_TankChangeEvent(Auto sender)
+        {
+            lblAuto.Content = a.ToString();
+        }
+
+        private void a_TankOpEvent(Auto sender)
+        {
+            wdwAuto.Background = new SolidColorBrush(Color.FromRgb(255, 0, 0));
+            MessageBox.Show("Tank is op!");
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            a.LitersInTank = a.LitersInTank - 1;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            timer = new DispatcherTimer(TimeSpan.FromSeconds(1),DispatcherPriority.Normal, timer_Tick, Dispatcher.CurrentDispatcher);
+
+            a = new Auto(14);
+            a.AutoGestartEvent += a_AutoGestartEvent;
+            a.AutoGestoptEvent += a_AutoGestopEvent;
+            a.TankBijnaOpEvent += a_TankBijnaOpEvent;
+            a.TankChangeEvent += a_TankChangeEvent;
+            a.TankOpEvent += a_TankOpEvent;
+            a.Snelheid = 50;
         }
     }
 }
